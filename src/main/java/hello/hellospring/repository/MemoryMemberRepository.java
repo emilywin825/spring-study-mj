a73883a44edc957ed62335e8cfcd45efb68f357f
emilywin825 <emilywin825@naver.com>
2023-10-17 06:52:51 +0900

    repository test기법

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,36 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+/**
+ * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
+ */
+public class MemoryMemberRepository implements MemberRepository{
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); //null이 반환 될 가능성이 있다면 이렇게 처리 -> 클라에서 처리 가능
+    }
+    @Override
+    public List<Member> findAll() { //자바 실무에서 List 많이 사용
+        return new ArrayList<>(store.values());
+    }
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny(); //끝까지 돌렸는데 null이면 Optional에 감싸져서 return 됨
+    }
+    public void clearStore() {
+        store.clear();
+    }
+}
---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,57 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.Optional;
+import static org.assertj.core.api.Assertions.*;
+class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach //메소드 끝날 때마다 리셋
+    public void afterEach() {
+        repository.clearStore();
+    }
+    @Test
+    @DisplayName("새로운 멤버를 save를 통해 추가하고 아이디 번호를 이용해 다시 찾으면 일치할 것이다.")
+    public void save() {
+        //given : 테스트를 위해 주어질 데이터
+        Member member = new Member();
+        member.setName("spring");
+        //when : 테스트 실제 상황
+        repository.save(member);
+        //then : 테스트 결과 확인
+        Member result = repository.findById(member.getId()).get();
+        assertThat(result).isEqualTo(member); //result가 member와 같은지를 테스트
+    }
+    @Test
+    public void findByName() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+        //when
+        Member result = repository.findByName("spring1").get();
+        //then
+        assertThat(result).isEqualTo(member1);
+    }
+    @Test
+    public void findAll() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+        //when
+        List<Member> result = repository.findAll();
+        //then
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
